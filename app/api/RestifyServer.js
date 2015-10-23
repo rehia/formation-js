0ab998e99ecba48c3da5b3a086aee2607430ef5d
Jérôme Avoustin <jerome@rehia.fr>
2015-10-23 23:44:43 +0200

    refactor(api): extract restify server in its own class

---
+++ app/api/RestifyServer.js
@@ -0,0 +1,50 @@
+/* jslint node: true */
+"use strict";
+
+var restify = require('restify');
+
+function RestifyServer(fields, hostname, port) {
+  this.fields = fields;
+  this.hostname = hostname;
+  this.port = port;
+}
+
+RestifyServer.prototype.setUp = function setUp() {
+  var server = restify.createServer({
+    name: this.hostname
+  });
+
+  server.use(restify.bodyParser());
+  server.use(restify.queryParser());
+
+  server.get('/fields/:id', function (request, response, next) {
+    try {
+      response.send(this.fields.getField(request.params.id));
+      return next();
+    } catch(error) {
+      return next(new restify.ResourceNotFoundError(error.message));
+    }
+  }.bind(this));
+
+  server.post('/fields', function (request, response, next) {
+    var id = this.fields.newField(request.params.state);
+    response.send(id);
+    next();
+  }.bind(this));
+
+  server.put('/fields/:id', function (request, response, next) {
+    try {
+      this.fields.updateField(request.params.id, request.params.state);
+      response.end();
+      return next();
+    } catch(error) {
+      return next(new restify.ResourceNotFoundError(error.message));
+    }
+  }.bind(this));
+
+  server.listen(this.port);
+
+  console.log('Server listening on port ' + this.port);
+};
+
+module.exports = RestifyServer;
--- app/api/server.js
+++ app/api/server.js
@@ -1,45 +1,11 @@
 'use strict';
 
-var restify = require('restify');
-
+var RestifyServer = require('./RestifyServer');
 var Fields = require('./Fields');
 
-var server = restify.createServer({
-  name: 'localhost'
-});
-
-server.use(restify.bodyParser());
-server.use(restify.queryParser());
-
 var fields = new Fields();
-
-server.get('/fields/:id', function (request, response, next) {
-  try {
-    response.send(fields.getField(request.params.id));
-    return next();
-  } catch(error) {
-    return next(new restify.ResourceNotFoundError(error.message));
-  }
-});
-
-server.post('/fields', function (request, response, next) {
-  var id = fields.newField(request.params.state);
-  response.send(id);
-  next();
-});
-
-server.put('/fields/:id', function (request, response, next) {
-  try {
-    fields.updateField(request.params.id, request.params.state);
-    response.end();
-    return next();
-  } catch(error) {
-    return next(new restify.ResourceNotFoundError(error.message));
-  }
-});
-
+var hostname = 'localhost';
 var port = 8082;
 
-server.listen(port);
-
-console.log('Server listening on port ' + port);
+var restifyServer = new RestifyServer(fields, hostname, port);
+restifyServer.setUp();
